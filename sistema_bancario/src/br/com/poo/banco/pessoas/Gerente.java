a090d08d87c885b4c17436c575070ab1263be21f
philippusv <felipebalbinoribeiro@gmail.com>
2023-09-05 15:50:26 -0300

    Construtores, getters e setters da classe Gerente

--- sistema_bancario/src/br/com/poo/banco/pessoas/Gerente.java
+++ sistema_bancario/src/br/com/poo/banco/pessoas/Gerente.java
@@ -1,9 +1,38 @@
 package br.com.poo.banco.pessoas;
 
 public class Gerente {
+	
+	//atributos
 	String nome;
 	String cpf;
 	double salario;
 	String senha;
 	int numFuncionarios;
+
+	//construtores
+	public Gerente() {
+	
+		}
+	
+	public Gerente(String nome, String cpf, double salario, String senha, int numFuncionarios) {
+		this.nome = nome;
+		this.cpf = cpf;
+		this.salario = salario;
+		this.senha = senha;
+		this.numFuncionarios = numFuncionarios;
+	}
+	
+	//getters
+	
+	public int getNumFuncionarios() {
+		return numFuncionarios;
+	}
+	
+	//setters
+	
+	public String setSenha() {
+		return senha;
+	}
+	
+	
 }
