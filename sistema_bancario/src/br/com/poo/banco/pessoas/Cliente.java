8321a23a237f0ff9235edebc1035298fb46f6669
Icaro <ikaro_460@hotmail.com>
2023-09-05 14:47:09 -0300

    feat: atributos, contrutores e getters cliente

--- sistema_bancario/src/br/com/poo/banco/pessoas/Cliente.java
+++ sistema_bancario/src/br/com/poo/banco/pessoas/Cliente.java
@@ -1,8 +1,31 @@
 package br.com.poo.banco.pessoas;
 
 public class Cliente {
-	
+
+	// atributos
 	String nome;
-	int cpf;
+	String cpf;
+	int numConta;
+
+	// construtores
+	public Cliente() {
+
+	}
+
+	public Cliente(String nome, String cpf, int numConta) {
+		this.nome = nome;
+		this.cpf = cpf;
+		this.numConta = numConta;
+	}
 	
+	// getters
+	public String getNome() {
+		return nome;
+	}
+	
+	// setters
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
 }
