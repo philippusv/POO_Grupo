563bb8089125343d3d604a9f69037389116d62aa
Icaro <ikaro_460@hotmail.com>
2023-09-15 03:53:57 -0300

    fix: Implementar novo mapa no JRelatorioGerente

--- sistema_bancario/src/br/com/poo/banco/views/JRelatorioGerente.java
+++ sistema_bancario/src/br/com/poo/banco/views/JRelatorioGerente.java
@@ -14,6 +14,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.util.List;
 import java.util.Map;
 
 import br.com.poo.banco.contas.Conta;
@@ -32,7 +33,6 @@ public class JRelatorioGerente extends JFrame {
 	private JTable tableContasAgencia;
 	private JTable tableInfoContas;
 
-	
 	/**
 	 * Criação de Frame
 	 */
@@ -46,32 +46,30 @@ public class JRelatorioGerente extends JFrame {
 
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
-		
-		
+
 		/*
 		 * Modelos de Tabelas
 		 */
-		String[] head_A = {"Nº da Conta", "Seleção"};
+		String[] head_A = { "Nº da Conta", "Seleção" };
 		DefaultTableModel model_A = new DefaultTableModel(head_A, 0) {
 			private static final long serialVersionUID = 1L;
-	
+
 			@Override
-		    public boolean isCellEditable(int row, int column) {
-		       return false;
-		    }	
+			public boolean isCellEditable(int row, int column) {
+				return false;
+			}
 		};
-		
-		String[] head_B = {"Nº da Conta", "Tipo de Conta", "Nome do Títular", "CPF do Títular", "Saldo"};
+
+		String[] head_B = { "Nº da Conta", "Tipo de Conta", "Nome do Títular", "CPF do Títular", "Saldo" };
 		DefaultTableModel model_B = new DefaultTableModel(head_B, 0) {
 			private static final long serialVersionUID = 1L;
-	
+
 			@Override
-		    public boolean isCellEditable(int row, int column) {
-		       return false;
-		    }	
+			public boolean isCellEditable(int row, int column) {
+				return false;
+			}
 		};
-		
-		
+
 		/*
 		 * Tabelas
 		 */
@@ -79,83 +77,72 @@ public class JRelatorioGerente extends JFrame {
 		tableInfoContas = new JTable(model_B);
 		tableInfoContas.setCellSelectionEnabled(true);
 		tableInfoContas.setFont(new Font("Tahoma", Font.PLAIN, 10));
-		
+
 		// Tabela para as contas da agência:
 		tableContasAgencia = new JTable(model_A);
 		tableContasAgencia.setCellSelectionEnabled(true);
 		tableContasAgencia.setFont(new Font("Tahoma", Font.PLAIN, 10));
 		tableContasAgencia.setBounds(10, 29, 414, 184);
-		
+
 		// gambiarra até a linha 156:
-		
+
 		// Adicionar todas as contas na agências:
-		for(Map.Entry<String, Conta> entry : ContaPoupanca.mapaContaPoupanca.entrySet()) {
-			Conta conta = entry.getValue();
-		    if(conta.getAgencia().equals(agencia)) {
-		    	String[] item = { conta.getNumero(), " " };
-		    	model_A.addRow(item);	
-		    } 
-		} for(Map.Entry<String, Conta> entry : ContaCorrente.mapaContaCorrente.entrySet()) {
-			Conta conta = entry.getValue();
-		    if(conta.getAgencia().equals(agencia)) {
-		    	String[] item = { conta.getNumero(), " " };
-		    	model_A.addRow(item);	
-		    }
+		for (Map.Entry<String, List<Conta>> entry : Conta.mapaContas.entrySet()) {
+			List<Conta> contas = entry.getValue();
+
+			for (Conta conta : contas) {
+
+				if (conta.getAgencia().equals(agencia)) {
+					String[] item = { conta.getNumero(), " " };
+					model_A.addRow(item);
+				}
+			}
 		}
-		
+
 		// Responder ao click do mouse:
 		tableContasAgencia.addMouseListener(new MouseAdapter() {
 			public void mouseClicked(MouseEvent me) {
-				JTable target = (JTable)me.getSource();
+				JTable target = (JTable) me.getSource();
 				int row = target.getSelectedRow(); // select a row
 				int column = target.getSelectedColumn(); // select a column
-				
+
 				if (column == 0) {
 					Object value = tableContasAgencia.getValueAt(row, column);
-					
+
 					// 2 Clicks:
 					if (me.getClickCount() == 2) {
-						
+
 						// Adicionar ou remover informações das contas selecionadas
 						if (tableContasAgencia.getValueAt(row, 1) == "Selecionado") {
 							model_A.setValueAt("", row, 1);
-							
+
 							for (int i = 0; i < tableInfoContas.getRowCount(); i++) {
-						        if (value == tableInfoContas.getValueAt(i, 0)) {
-						        	model_B.removeRow(i);
-						        	break;
-						        }
-						    }
+								if (value == tableInfoContas.getValueAt(i, 0)) {
+									model_B.removeRow(i);
+									break;
+								}
+							}
 						} else {
 							model_A.setValueAt("Selecionado", row, 1);
-							for(Map.Entry<String, Conta> entry : ContaPoupanca.mapaContaPoupanca.entrySet()) {
-								Conta conta = entry.getValue();
-							    if(conta.getNumero().equals(value)) {
-							    	String[] item = { conta.getNumero(),
-							    			conta.getTipoConta(),
-							    			Pessoas.mapaPessoas.get(conta.getCpfTitular()).getNome(),
-							    			conta.getCpfTitular(),
-							    			"" + conta.getSaldo() 
-							    	}; model_B.addRow(item);	
-							    } 
-							} for(Map.Entry<String, Conta> entry : ContaCorrente.mapaContaCorrente.entrySet()) {
-								Conta conta = entry.getValue();
-							    if(conta.getNumero().equals(value)) {
-							    	String[] item = { conta.getNumero(),
-							    			conta.getTipoConta(),
-							    			Pessoas.mapaPessoas.get(conta.getCpfTitular()).getNome(),
-							    			conta.getCpfTitular(),
-							    			"" + conta.getSaldo() 
-							    	}; model_B.addRow(item);		
-							    }
+							for (Map.Entry<String, List<Conta>> entry : Conta.mapaContas.entrySet()) {
+								List<Conta> contas = entry.getValue();
+
+								for (Conta conta : contas) {
+
+									if (conta.getNumero().equals(value)) {
+										String[] item = { conta.getNumero(), conta.getTipoConta(),
+												Pessoas.mapaPessoas.get(conta.getCpfTitular()).getNome(),
+												conta.getCpfTitular(), "" + conta.getSaldo() };
+										model_B.addRow(item);
+									}
+								}
 							}
 						}
-					} 
+					}
 				}
 			}
 		});
-		
-		
+
 		/*
 		 * Botões
 		 */
@@ -164,13 +151,13 @@ public class JRelatorioGerente extends JFrame {
 		btnGerarRelatorio1.setFont(new Font("Tahoma", Font.PLAIN, 12));
 		btnGerarRelatorio1.setBounds(10, 244, 292, 26);
 		contentPane.add(btnGerarRelatorio1);
-		
+
 		// Imprimir relatório das contas selecionadas:
 		JButton btnGerarRelatorio2 = new JButton("Imprimir relatório das contas selecionadas");
 		btnGerarRelatorio2.setFont(new Font("Tahoma", Font.PLAIN, 12));
 		btnGerarRelatorio2.setBounds(312, 244, 292, 26);
 		contentPane.add(btnGerarRelatorio2);
-		
+
 		// Voltar para o menu do gerente:
 		JButton btnVoltar = new JButton("Voltar");
 		btnVoltar.addActionListener(new ActionListener() {
@@ -184,8 +171,7 @@ public class JRelatorioGerente extends JFrame {
 		btnVoltar.setFont(new Font("Tahoma", Font.PLAIN, 12));
 		btnVoltar.setBounds(614, 244, 120, 26);
 		contentPane.add(btnVoltar);
-		
-		
+
 		/*
 		 * Outros
 		 */
@@ -194,14 +180,14 @@ public class JRelatorioGerente extends JFrame {
 		lblNewLabel.setFont(new Font("Tahoma", Font.BOLD, 12));
 		lblNewLabel.setBounds(10, 11, 414, 14);
 		contentPane.add(lblNewLabel);
-		
+
 		// JScrolls
 		JScrollPane scrollConta = new JScrollPane();
 		scrollConta.setViewportBorder(new LineBorder(new Color(0, 0, 0)));
 		scrollConta.setBounds(10, 29, 220, 204);
 		contentPane.add(scrollConta);
 		scrollConta.setViewportView(tableContasAgencia);
-		
+
 		JScrollPane scrollData = new JScrollPane();
 		scrollData.setViewportBorder(new LineBorder(new Color(0, 0, 0)));
 		scrollData.setBounds(240, 29, 494, 204);
